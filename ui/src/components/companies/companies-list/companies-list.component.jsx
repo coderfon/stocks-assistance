1f63cb3197ba8da5b6af4c951bf68517c0016bc1
Alfonso Muñoz <alfonso.mg@outlook.com>
2023-05-22 00:29:43 +0200

    [UI] Table sorting

--- ui/src/components/companies/companies-list/companies-list.component.jsx
+++ ui/src/components/companies/companies-list/companies-list.component.jsx
@@ -13,6 +13,9 @@ const CompaniesList = () => {
     const [selectedSector, setSelectedSector] = useState('');
     const [selectedIndustry, setSelectedIndustry] = useState('');
 
+    const [sortingColumn, setSortingColumn] = useState('');
+    const [sortingDirection, setSortingDirection] = useState('');
+
     useEffect(() => {
         fetch('http://localhost:5071/api/Company/filter', {
             method: "POST",
@@ -36,16 +39,16 @@ const CompaniesList = () => {
             })
             .then((data) => {
                 setSectors(data.sectors);
-                const adaptedSectors = data.sectors.map(({ id, name }) => ({ id, name }));
-                adaptedSectors.splice(0, 0, { id: 0, name: "all"});
-                setSelectSectors(adaptedSectors)
-                setSelectedSector(adaptedSectors[0].id);
+                const filteredSectors = data.sectors.map(({ id, name }) => ({ id, name }));
+                filteredSectors.splice(0, 0, { id: 0, name: "all"});
+                setSelectSectors(filteredSectors)
+                setSelectedSector(filteredSectors[0].id);
 
                 setIndustries(data.industries);
-                const adaptedIndustries = data.industries.map(({ id, name }) => ({ id, name }));
-                adaptedIndustries.splice(0, 0, { id: 0, name: "all"});
-                setSelectIndustries(adaptedIndustries);
-                setSelectedIndustry(adaptedIndustries[0].id);
+                const filteredIndustries = data.industries.map(({ id, name }) => ({ id, name }));
+                filteredIndustries.splice(0, 0, { id: 0, name: "all"});
+                setSelectIndustries(filteredIndustries);
+                setSelectedIndustry(filteredIndustries[0].id);
 
                 setTags(data.tags);
             });
@@ -56,26 +59,76 @@ const CompaniesList = () => {
     }
 
     const sectorChangeHandler = (value) => {
-        let adaptedIndustries = [];
-        console.log(industries);
-        if(value !== 0) {
-            adaptedIndustries = industries.filter(function(item) {
-                return item.sectorId === value
-            })
+        let filteredIndustries = [];
+        let intVal = parseInt(value);
+
+        if(intVal !== 0) {
+            filteredIndustries = industries.filter(item => item.sectorId === intVal)
             .map(({ id, name }) => ({ id, name }));
         }
         else {
-            adaptedIndustries = industries.map(({ id, name }) => ({ id, name }));
-        }
-          
-        console.log(adaptedIndustries);
+            filteredIndustries = industries.map(({ id, name }) => ({ id, name }));
+        } 
 
-        adaptedIndustries.splice(0, 0, { id: 0, name: "all"});
+        filteredIndustries.splice(0, 0, { id: 0, name: "all"});
         
-        setSelectIndustries(adaptedIndustries);       
-        setSelectedIndustry(adaptedIndustries[0].id);
+        setSelectIndustries(filteredIndustries);   
+        setSelectedSector(intVal);    
+        setSelectedIndustry(filteredIndustries[0].id);     
+    }
 
-        setSelectedSector(value);
+    const sortCompanies = (column) => {
+        let lastSortingColumn = sortingColumn;
+        setSortingDirection(lastSortingColumn !== column ? 'asc' : sortingDirection === 'desc' ? 'asc' : 'desc');
+        setSortingColumn(column);
+        setCompanies(companies.sort(compare));
+    }
+
+    const shorten = (value) => {
+        if(value.length > 20) {
+            return value.substring(0,20) + '...';
+        }
+        return value;
+    }
+
+    const compare = (a, b) => {
+
+        if(sortingColumn !== 'sector' && sortingColumn !== 'industry') {
+            if(sortingDirection === 'asc'){
+                if(Reflect.get(a, sortingColumn) < Reflect.get(b, sortingColumn)) return -1;
+                if(Reflect.get(a, sortingColumn) > Reflect.get(b, sortingColumn)) return 1;
+            }
+            else {
+                if(Reflect.get(a, sortingColumn) < Reflect.get(b, sortingColumn)) return 1;
+                if(Reflect.get(a, sortingColumn) > Reflect.get(b, sortingColumn)) return -1;
+            }
+        }
+        else {
+            switch(sortingColumn) {
+                case 'sector':
+                    if(sortingDirection === 'asc'){
+                        if(a.sector.name < b.sector.name) return -1;
+                        if(a.sector.name > b.sector.name) return 1;
+                    }
+                    else {
+                        if(a.sector.name < b.sector.name) return 1;
+                        if(a.sector.name > b.sector.name) return -1;
+                    }
+                    break;
+                case 'industry':
+                    if(sortingDirection === 'asc'){
+                        if(a.sector.name < b.industry.name) return -1;
+                        if(a.sector.name > b.industry.name) return 1;
+                    }
+                    else {
+                        if(a.sector.name < b.industry.name) return 1;
+                        if(a.sector.name > b.industry.name) return -1;
+                    }
+                    break;
+            }
+        }
+
+        return 0;
     }
 
     return (
@@ -101,22 +154,66 @@ const CompaniesList = () => {
                 <thead>
                     <tr>
                         <th>
-                            Name
+                            <span onClick={e => sortCompanies('symbol')}>Symbol</span>
                         </th>
                         <th>
-                            Industry
+                            <span onClick={e => sortCompanies('name')}>Name</span>
                         </th>
                         <th>
-                            Sector
+                            <span onClick={e => sortCompanies('sector')}>Sector</span>
+                        </th>
+                        <th>
+                            <span onClick={e => sortCompanies('industry')}>Industry</span>
+                        </th>
+                        <th>
+                            <span onClick={e => sortCompanies('country')}>Country</span>
+                        </th>
+                        <th>
+                            <span onClick={e => sortCompanies('price')}>Price</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('last52WeekHigh')}>52WH</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('last52WeekLow')}>52WL</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('marketCap')}>MCap</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('ltM_PE')}>LTM PE</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('ltM_PE')}>NTM PE</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('priceBookRatio')}>P/B</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('dividendYield')}>Div %</span>
+                        </th>
+                        <th>
+                        <span onClick={e => sortCompanies('roe')}>ROE</span>
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     {companies?.map((company) => (
                         <tr key={company.id}>
-                            <td>{company.name}</td>
+                            <td>{company.symbol}</td>
+                            <td>{shorten(company.name)}</td>
                             <td>{company.sector.name}</td>
                             <td>{company.industry.name}</td>
+                            <td>{company.country}</td>
+                            <td>{company.price}</td>
+                            <td>{company.last52WeekHigh}</td>
+                            <td>{company.last52WeekLow}</td>
+                            <td>{company.marketCap}</td>
+                            <td>{company.ltM_PE.toFixed(2)}</td>
+                            <td>{company.ltM_PE.toFixed(2)}</td>
+                            <td>{company.priceBookRatio.toFixed(2)}</td>
+                            <td>{(company.dividendYield * 100).toFixed(2)}%</td>
+                            <td>{(company.roe * 100).toFixed(2)}%</td>
                         </tr>
                     ))}
                 </tbody>
